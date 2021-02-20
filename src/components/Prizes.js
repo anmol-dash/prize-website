bb178996743e3b9c6dd4013819545d4b3c94a35d
Chirality <danieltaox@gmail.com>
2021-02-20 16:28:48 -0500

    huge clout outcome description section

---
+++ src/components/Prizes.js
@@ -0,0 +1,39 @@
+import React from "react"
+import s from "styled-components"
+import { Title, Subtitle } from "./Typography"
+
+const LevelItem = s.div.attrs(() => ({
+  className: "level-item has-text-centered"
+}))`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+`
+
+const Level = s.div`
+  margin-top: 2vw;
+`
+
+const PrizesLayout = ({ className }) => (
+  <div className={`container is-fluid has-text-centered ${className}`}>
+    <Subtitle>Dive into the most pressing environmental issues of today.</Subtitle>
+    <Level className={`level is-mobile`}>
+      <LevelItem>
+        <Subtitle>First Place Grand Prize</Subtitle>
+        <Title>$5000</Title>
+      </LevelItem>
+      <LevelItem>
+        <Subtitle>Second</Subtitle>
+        <Title>$2500</Title>
+      </LevelItem>
+      <LevelItem>
+        <Subtitle>Third</Subtitle>
+        <Title>$1250</Title>
+      </LevelItem>
+    </Level>
+  </div>
+)
+
+export const Prizes = s(PrizesLayout)`
+  margin-top: 10vw;
+`
