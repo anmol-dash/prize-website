63e1950e7fefcc9b4bb3d6df46a99b78d23b5bb9
Chirality <danieltaox@gmail.com>
2021-02-20 16:27:37 -0500

    shared typography style utils

---
+++ src/components/Typography.js
@@ -0,0 +1,17 @@
+import s from "styled-components"
+
+export const Title = s.h1`
+  font-size: 8rem;
+`
+
+export const Subtitle = s.h2`
+  font-size: 3.5rem;
+`
+
+export const Text = s.p`
+  font-size: 3rem;
+`
+
+export const SubText = s.p`
+  font-size: 2rem;
+`
