a48905bfb71da7b0fc14aa518c460d51d2b3d6db
Chirality <danieltaox@gmail.com>
2021-02-20 16:28:16 -0500

    huge first impressions layout thing section

---
+++ src/components/Hero.js
@@ -0,0 +1,34 @@
+import React from "react"
+import s from "styled-components"
+import { Title, Subtitle } from "./Typography"
+
+const HeroTitle = s(Title)`
+  margin-top: -2rem;
+`
+
+const LeftMargin = Component => s(Component)`
+  margin-left: 4rem;
+`
+const SpacedTitle = LeftMargin(Title)
+const SpacedSubtitle = LeftMargin(Subtitle)
+
+const InlineBlock = s.div`
+  text-align: left;
+  display: inline-block;
+`
+
+const HeroLayout = ({ className }) => (
+  <div className={`container is-fluid has-text-centered ${className}`}>
+    <InlineBlock>
+      <SpacedTitle>APRIL 3rd - 17th</SpacedTitle>
+      <HeroTitle>Penn Climate Ventures Prize</HeroTitle>
+      <SpacedSubtitle>building out Penn's climate innovation ecosystem</SpacedSubtitle>
+      <SpacedSubtitle>bringing environmental impact into Penn's startup scene</SpacedSubtitle>
+      <SpacedSubtitle>breaking Penn's talent into a critical space</SpacedSubtitle>
+    </InlineBlock>
+  </div>
+)
+
+export const Hero = s(HeroLayout)`
+  margin-top: 10vw;
+`
