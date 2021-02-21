ed4d6b2b4a4cd287e4447e4fe23bec6542d48147
Chirality <danieltaox@gmail.com>
2021-02-20 20:53:02 -0500

    update Typography with new font family styling

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -2,16 +2,21 @@ import s from "styled-components"
 
 export const Title = s.h1`
   font-size: 8rem;
+  font-family: Roboto;
 `
 
 export const Subtitle = s.h2`
   font-size: 3.5rem;
+  font-family: Metropolis;
+  font-weight: bold;
 `
 
 export const Text = s.p`
   font-size: 3rem;
+  font-family: Metropolis;
 `
 
 export const SubText = s.p`
   font-size: 2rem;
+  font-family: Metropolis
 `
