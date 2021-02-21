6d729a6d631dbb529566c66d5a48a7ff701ad96a
Chirality <danieltaox@gmail.com>
2021-02-20 19:13:17 -0500

    refactor Timeline into its own folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export * from './Typography'
 export * from './Hero'
-export * from './Prizes'
+export * from './Prizes';
+export * from './Timeline'
 export * from './Donors'
 export * from './Footer'
