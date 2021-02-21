c59e5473f7e031d085b01ff68bc4ce17e123bab4
Chirality <danieltaox@gmail.com>
2021-02-20 22:30:10 -0500

    allow fontSize prop to override Typography components

--- src/components/shared/Typography.js
+++ src/components/shared/Typography.js
@@ -1,22 +1,22 @@
 import s from "styled-components"
 
 export const Title = s.h1`
-  font-size: 8rem;
+  font-size: ${({ fontSize }) => fontSize || '8rem'};
   font-family: Roboto;
 `
 
 export const Subtitle = s.h2`
-  font-size: 3.5rem;
+  font-size: ${({ fontSize }) => fontSize || '3.5rem'};
   font-family: Metropolis;
   font-weight: bold;
 `
 
 export const Text = s.p`
-  font-size: 3rem;
+  font-size: ${({ fontSize }) => fontSize || '3rem'};
   font-family: Metropolis;
 `
 
 export const SubText = s.p`
-  font-size: 2rem;
+  font-size: ${({ fontSize }) => fontSize || '2rem'};
   font-family: Metropolis
 `
