d3257243759d2c8b303f7df14f05cebc9d22291c
Chirality <danieltaox@gmail.com>
2021-02-25 03:24:11 -0500

    update to Roboto Condensed

--- src/components/shared/Typography.js
+++ src/components/shared/Typography.js
@@ -1,22 +1,22 @@
 import s from "styled-components"
 
 export const Title = s.h1`
-  font-size: ${({ fontSize }) => fontSize || '8rem'};
+  font-size: ${({ fontSize }) => fontSize || '10rem'};
   font-family: Roboto Condensed; 
 `
 
 export const Subtitle = s.h2`
-  font-size: ${({ fontSize }) => fontSize || '3.5rem'};
-  font-family: Metropolis;
+  font-size: ${({ fontSize }) => fontSize || '4rem'};
+  font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
   font-weight: bold;
 `
 
 export const Text = s.p`
   font-size: ${({ fontSize }) => fontSize || '3rem'};
-  font-family: Metropolis;
+  font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
 `
 
 export const SubText = s.p`
   font-size: ${({ fontSize }) => fontSize || '2rem'};
-  font-family: Metropolis
+  font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
 `
