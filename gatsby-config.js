b37d3178e3ad79da89b2206044edab892639a595
Chirality <danieltaox@gmail.com>
2021-02-27 19:33:36 -0500

    add favicon and other metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,5 +22,19 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Penn Climate Ventures Prize",
+        short_name: "PCV Prize",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#3083BC",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.png" // This path is relative to the root of the site.
+      }
+    }
   ],
 };
