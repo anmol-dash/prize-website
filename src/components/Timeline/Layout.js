61b4c7f47a25354dd7aa250c4134ad25c1b81de7
Chirality <danieltaox@gmail.com>
2021-03-03 18:44:29 -0500

    remove timeline line on mobile

--- src/components/Timeline/Layout.js
+++ src/components/Timeline/Layout.js
@@ -1,19 +1,29 @@
-import React from "react"
 import s from "styled-components"
-import { Title, Text, Subtitle, SubText } from "../shared/Typography"
-import { MIDNIGHT_BLUE } from "../../utils/constants"
+import { MIDNIGHT_BLUE, mediaMaxWidth } from "../../utils/constants"
 
 export const Columns = s.div.attrs(() => ({
     className: "columns"
 }))`
-:before {
-    content: '';
-    position: absolute;
-    top: -.1rem;
-    width: 6%;
-    left: 47%;
-    border-bottom: .4rem solid ${MIDNIGHT_BLUE};
-}
+    ${mediaMaxWidth("768px")} {
+        padding: 4vw;
+    }
+
+    :before {
+        content: '';
+        position: absolute;
+        top: -.1rem;
+        width: 6%;
+        left: 47%;
+        right: 47%;
+        border-bottom: .4rem solid ${MIDNIGHT_BLUE};
+
+        ${mediaMaxWidth("768px")} {
+            width: 26%;
+            left: 37%;
+            right: 37%;
+            border-bottom: .2rem solid ${MIDNIGHT_BLUE};
+        }
+    }
 `
 
 export const Col = s.div.attrs(() => ({
@@ -24,10 +34,18 @@ export const Col = s.div.attrs(() => ({
 
 export const Left = s(Col)`
     border-right: .3rem solid ${MIDNIGHT_BLUE};
+    ${mediaMaxWidth("768px")} {
+        border: none !important;
+        display: contents;
+    }
 `
 
 export const Right = s(Col)`
     border-left: .3rem solid ${MIDNIGHT_BLUE};
+    ${mediaMaxWidth("768px")} {
+        border: none !important;
+        display: contents;
+    }
 `
 
 export const Spacer = s.div`
