e061c9d9bacea9945e55cd0877e8162772c8c372
Chirality <danieltaox@gmail.com>
2021-03-03 01:58:31 -0500

    more responsive styling

--- src/components/shared/Typography.js
+++ src/components/shared/Typography.js
@@ -1,22 +1,22 @@
 import s from "styled-components"
 
 export const Title = s.h1`
-  font-size: ${({ fontSize }) => fontSize || '8rem'};
+  font-size: calc(${({ fontSize }) => fontSize || '4rem'} + 2vw);
   font-family: Roboto Condensed; 
 `
 
 export const Subtitle = s.h2`
-  font-size: ${({ fontSize }) => fontSize || '3.5rem'};
+  font-size: calc(${({ fontSize }) => fontSize || '2rem'} + 1vw);
   font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
   font-weight: bold;
 `
 
 export const Text = s.p`
-  font-size: ${({ fontSize }) => fontSize || '2.5rem'};
+  font-size: calc(${({ fontSize }) => fontSize || '1rem'} + 1vw);
   font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
 `
 
 export const SubText = s.p`
-  font-size: ${({ fontSize }) => fontSize || '2rem'};
+  font-size: calc(${({ fontSize }) => fontSize || '.75rem'} + 1vw);
   font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
 `
