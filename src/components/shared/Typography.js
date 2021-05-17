32ce24695c292433620fbd50c44284864259096d
nicole-chau <74137085+nicole-chau@users.noreply.github.com>
2021-05-17 23:14:29 +0800

    updated fonts

--- src/components/shared/Typography.js
+++ src/components/shared/Typography.js
@@ -1,22 +1,30 @@
 import s from "styled-components"
 
 export const Title = s.h1`
-  font-size: calc(${({ fontSize }) => fontSize || '4rem'} + 2vw);
-  font-family: Roboto Condensed; 
+  font-size: calc(${({ fontSize }) => fontSize || '2rem'} + 2vw);
+  font-family: Roboto;
+  font-weight: bold;
+  color: #3083BC 
 `
 
 export const Subtitle = s.h2`
-  font-size: calc(${({ fontSize }) => fontSize || '2rem'} + 1vw);
-  font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
+  font-size: calc(${({ fontSize }) => fontSize || '1rem'} + 1vw);
+  font-family: ${({avenir}) => avenir ? `Avenir` : `Roboto`};
+  font-weight: bold;
+`
+
+export const Heading3 = s.h3`
+  font-size: calc(${({ fontSize }) => fontSize || '0.75rem'} + 1vw);
+  font-family: ${({avenir}) => avenir ? `Avenir` : `Roboto`};
   font-weight: bold;
 `
 
 export const Text = s.p`
-  font-size: calc(${({ fontSize }) => fontSize || '1rem'} + 1vw);
-  font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
+  font-size: calc(${({ fontSize }) => fontSize || '0.4rem'} + 1vw);
+  font-family: ${({roboto}) => roboto ? `Roboto` : `Avenir`};
 `
 
 export const SubText = s.p`
-  font-size: calc(${({ fontSize }) => fontSize || '.75rem'} + 1vw);
-  font-family: ${({roboto}) => roboto ? `Roboto Condensed` : `Metropolis`};
+  font-size: calc(${({ fontSize }) => fontSize || '.3rem'} + 1vw);
+  font-family: ${({roboto}) => roboto ? `Roboto` : `Avenir`};
 `
