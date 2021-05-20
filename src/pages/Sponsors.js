a97c46714c4beae11668086782a43c7559e326c3
nicole-chau <74137085+nicole-chau@users.noreply.github.com>
2021-05-20 23:36:52 +0800

    added alt tag and return statement

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,39 +1,41 @@
 import React from "react"
-import s from "styled-components"
 import Kleinman from '../images/kleinman.png'
 import MT from '../images/M&T_Logo_Tagline.png'
 import SP2 from '../images/sp2logo.png'
 import WGY from '../images/Whartonglobalyouthprogram.png'
 
 
-import { Title, Text, Subtitle, SubText } from "../components/shared/Typography"
-import Button from "../components/shared/Button"
+import { Title, Text, Subtitle } from "../components/shared/Typography"
 import { Navbar } from "../components/Navbar"
 
-<div>
-<Navbar/>
-    <Title>
-        Sponsors
-    </Title>
+const Sponsors = () => {
+    return (
+        <div>
+        <Navbar/>
+            <Title>
+                Sponsors
+            </Title>
 
-    <Text>
-        Helping push the next generation into climate with quick, catalytic capital.
-    </Text>
+            <Text>
+                Helping push the next generation into climate with quick, catalytic capital.
+            </Text>
 
-    <Subtitle>
-        Our Supporters for the 2021 PCV Prize:
-    </Subtitle>
+            <Subtitle>
+                Our Supporters for the 2021 PCV Prize:
+            </Subtitle>
 
-    <img src={SP2} />
-    <br/>
-    <img src={Kleinman} />
-    <br/>
-    <img src={WGY} />
-    <br/>
-    <img src={MT} />
-    <br/>
-    
-    <Text>
-        If you would like to become a sponsor of our 2022 Prize, please shoot us an email at pennclimateventures@gmail.com!
-    </Text>
-</div>
+            <img src={SP2} alt="SP2"/>
+            <br/>
+            <img src={Kleinman} alt="Kleinman"/>
+            <br/>
+            <img src={WGY} alt="WGY"/>
+            <br/>
+            <img src={MT} alt="MT"/>
+            <br/>
+            
+            <Text>
+                If you would like to become a sponsor of our 2022 Prize, please shoot us an email at pennclimateventures@gmail.com!
+            </Text>
+        </div>        
+    )
+}
