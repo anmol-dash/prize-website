a1d6e592f729a90b6229f610c05b115aa3d83a3f
ryoma <84463842+hryoma@users.noreply.github.com>
2022-01-15 15:09:07 -0500

    Past competitions page

--- src/pages/2021/people.js
+++ src/pages/2021/people.js
@@ -1,34 +1,34 @@
 import React from "react"
 import s from "styled-components"
 import Helmet from "react-helmet"
-import "../pages/styles.scss"
-import { Title, Text, Subtitle} from "../components/shared/Typography"
-import { Navbar, Footer } from "../components"
-import Headshot from "../components/shared/Headshot"
+import "../styles.scss"
+import { Title, Text, Subtitle} from "../../components/shared/Typography"
+import { Navbar, Footer } from "../../components"
+import Headshot from "../../components/shared/Headshot"
 
-import ArunMajumdar from "../images/2021/keynote/ArunMajumdar.jpg"
-import DennisWoodside from "../images/2021/keynote/DennisWoodside.jpg"
+import ArunMajumdar from "../../images/2021/keynote/ArunMajumdar.jpg"
+import DennisWoodside from "../../images/2021/keynote/DennisWoodside.jpg"
 
-import ZhengxiaDou from "../images/2021/panelists/ZhengxiaDou.jpg"
-import DavdGalligan from "../images/2021/panelists/DavidGalligan.jpg"
-import JamesHagan from "../images/2021/panelists/JamesHagan.jpg"
-import PeterPsarras from "../images/2021/panelists/PeterPsarras.jpg"
-import ArthurBenthem from "../images/2021/panelists/ArthurBenthem.jpg"
-import ESwann from "../images/2021/panelists/EMitchellSwann.jpg"
-import AlexBehar from "../images/2021/panelists/AlexBehar.jpg"
-import ShuoYang from "../images/2021/panelists/ShuoYang.jpg"
-import AllyWarson from "../images/2021/panelists/AllyWarson.jpg"
-import KimberlyZou from "../images/2021/panelists/KimberlyZou.jpg"
-import RyFisher from "../images/2021/panelists/RyStoreyFisher.jpg"
-import FranzHochstrasser from "../images/2021/panelists/FranzHochstrasser.jpg"
+import ZhengxiaDou from "../../images/2021/panelists/ZhengxiaDou.jpg"
+import DavdGalligan from "../../images/2021/panelists/DavidGalligan.jpg"
+import JamesHagan from "../../images/2021/panelists/JamesHagan.jpg"
+import PeterPsarras from "../../images/2021/panelists/PeterPsarras.jpg"
+import ArthurBenthem from "../../images/2021/panelists/ArthurBenthem.jpg"
+import ESwann from "../../images/2021/panelists/EMitchellSwann.jpg"
+import AlexBehar from "../../images/2021/panelists/AlexBehar.jpg"
+import ShuoYang from "../../images/2021/panelists/ShuoYang.jpg"
+import AllyWarson from "../../images/2021/panelists/AllyWarson.jpg"
+import KimberlyZou from "../../images/2021/panelists/KimberlyZou.jpg"
+import RyFisher from "../../images/2021/panelists/RyStoreyFisher.jpg"
+import FranzHochstrasser from "../../images/2021/panelists/FranzHochstrasser.jpg"
 
-import CaitlynMcCloskey from "../images/2021/moderators/CaitlynMcCloskey.jpg"
-import BenMay from "../images/2021/moderators/BenMay.jpg"
-import AkaashPadmanabha from "../images/2021/moderators/AkaashPadmanabha.jpg"
+import CaitlynMcCloskey from "../../images/2021/moderators/CaitlynMcCloskey.jpg"
+import BenMay from "../../images/2021/moderators/BenMay.jpg"
+import AkaashPadmanabha from "../../images/2021/moderators/AkaashPadmanabha.jpg"
 
-import PEGLogo from "../images/2021/cosponsors/peg.jpg"
-import SSAPLogo from "../images/2021/cosponsors/ssap.jpg"
-import PUCPLogo from "../images/2021/cosponsors/pucp.jpg"
+import PEGLogo from "../../images/2021/cosponsors/peg.jpg"
+import SSAPLogo from "../../images/2021/cosponsors/ssap.jpg"
+import PUCPLogo from "../../images/2021/cosponsors/pucp.jpg"
 
 const GridLayout = s.div`
   display: flex;
@@ -42,13 +42,13 @@ const Keynote = () => {
     <div>
       <Subtitle>Keynote Speakers</Subtitle>
       <GridLayout>
-        <Headshot 
-          imageSrc={ArunMajumdar} 
+        <Headshot
+          imageSrc={ArunMajumdar}
           imageAlt="Arun Majumdar"
           name="Arun Majumdar"
           description={<>Jay Precourt Professor of Mechanical Engineering and Photon Science and by courtesy, of Materials Science and Engineering, Stanford University</>}/>
-        <Headshot 
-          imageSrc={DennisWoodside} 
+        <Headshot
+          imageSrc={DennisWoodside}
           imageAlt="Dennis Woodside"
           name="Dennis Woodside"
           description="President of Impossible Foods" />
@@ -63,74 +63,74 @@ const Panelists = () => {
     <div>
       <Subtitle>Panelists</Subtitle>
       <GridLayout>
-        <Headshot 
-          imageSrc={ZhengxiaDou} 
+        <Headshot
+          imageSrc={ZhengxiaDou}
           imageAlt="Dr. Zhengxia Dou"
           name="Dr. Zhengxia Dou"
           description="Professor, Agricultural Systems, University of Pennsylvania School of Veterinary Medicine, Department of Clinical Studies, New Bolton Center" />
-  
-        <Headshot 
-          imageSrc={DavdGalligan} 
+
+        <Headshot
+          imageSrc={DavdGalligan}
           imageAlt="Dr. David T Galligan"
           name="Dr. David T Galligan"
           description="Professor of Animal Health Economics, University of Pennsylvania, School of Veterinary Medicine" />
-      
-        <Headshot 
-          imageSrc={JamesHagan} 
-          imageAlt="Dr. James R Hagan" 
-          name="Dr. James R Hagan" 
+
+        <Headshot
+          imageSrc={JamesHagan}
+          imageAlt="Dr. James R Hagan"
+          name="Dr. James R Hagan"
           description="Graduate Student Advisor in the Sustainability and Environmental Policy concentrations, Former Vice President of Sustainability & Environment at GlaxoSmithKline"/>
-      
-        <Headshot 
-          imageSrc={PeterPsarras} 
+
+        <Headshot
+          imageSrc={PeterPsarras}
           imageAlt="Dr. Peter Psarras"
           name="Dr. Peter Psarras"
           description="Research Assistant Professor, Chemical and Biomolecular Engineering" />
-      
-        <Headshot 
-          imageSrc={ArthurBenthem} 
+
+        <Headshot
+          imageSrc={ArthurBenthem}
           imageAlt="Dr. Arthur van Benthem"
           name="Dr. Arthur van Benthem"
           description="Associate Professor of Business Economics and Public Policy" />
-      
-        <Headshot 
-          imageSrc={ESwann} 
+
+        <Headshot
+          imageSrc={ESwann}
           imageAlt="E. Mitchell Swann"
           name="E. Mitchell Swann"
           description="Managing Director at Resolution Management Consultants, Philadelphia Energy Authority Board" />
-      
-        <Headshot 
-          imageSrc={AlexBehar} 
+
+        <Headshot
+          imageSrc={AlexBehar}
           imageAlt="Alex Behar"
           name="Alex Behar"
           description="Vice President at Cultivian Sandbox" />
-      
-        <Headshot 
-          imageSrc={ShuoYang} 
+
+        <Headshot
+          imageSrc={ShuoYang}
           imageAlt="Shuo Yang"
           name="Shuo Yang"
           description="Principal at Fifty Years" />
 
-        <Headshot 
-          imageSrc={AllyWarson} 
+        <Headshot
+          imageSrc={AllyWarson}
           imageAlt="Ally Warson"
           name="Ally Warson"
           description="Senior Associate at UP Partners" />
 
-        <Headshot 
-          imageSrc={KimberlyZou} 
-          imageAlt="Kimberly Zou" 
+        <Headshot
+          imageSrc={KimberlyZou}
+          imageAlt="Kimberly Zou"
           name="Kimberly Zou"
           description="Founder of Climate Tech VC and Analyst at Energy Impact Partners"/>
 
-        <Headshot 
-          imageSrc={RyFisher} 
+        <Headshot
+          imageSrc={RyFisher}
           imageAlt="Ry Storey-Fisher"
           name="Ry Storey-Fisher"
           description="Senior Partnership Associate at Powerhouse" />
 
-        <Headshot 
-          imageSrc={FranzHochstrasser} 
+        <Headshot
+          imageSrc={FranzHochstrasser}
           imageAlt="Franz Hochstrasser"
           name="Franz Hochstrasser"
           description="CEO and Co-founder of Raise Green and New Haven Community Solar, Former Senior Advisor to the Special Envoy for Climate Change @ the US State Dept" />
@@ -145,24 +145,24 @@ const Moderators = () => {
     <div>
       <Subtitle>Moderators</Subtitle>
       <GridLayout>
-        <Headshot 
-          imageSrc={CaitlynMcCloskey} 
+        <Headshot
+          imageSrc={CaitlynMcCloskey}
           imageAlt="Caitlyn McCloskey"
           name="Caitlyn McCloskey"
           description={<>Executive member of the <br/> Penn Environmental Group</>} />
 
-        <Headshot 
-          imageSrc={BenMay} 
+        <Headshot
+          imageSrc={BenMay}
           imageAlt="Ben May"
           name="Ben May"
-          description={<>Founder of ThinkOcean, 
-            <br/> 
-            Former Co-Chair of SSAP, 
+          description={<>Founder of ThinkOcean,
+            <br/>
+            Former Co-Chair of SSAP,
             <br/>
             Former UA Director of Sustainability and Community Impact</>} />
 
-        <Headshot 
-          imageSrc={AkaashPadmanabha} 
+        <Headshot
+          imageSrc={AkaashPadmanabha}
           imageAlt="Akaash Padmanabha"
           name="Akaash Padmanabha"
           description="Chair of Penn Dining Advisory Board" />
@@ -177,20 +177,20 @@ const CoSponsors = () => {
     <div>
       <Subtitle>Event Co-Sponsors</Subtitle>
       <GridLayout>
-        <Headshot 
-          imageSrc={PEGLogo} 
+        <Headshot
+          imageSrc={PEGLogo}
           imageAlt="Penn Environmental Group"
           name="Penn Environmental Group"
           description="Penn's oldest and largest environmental group, addressing environmental issues and increasing environmental awareness both at a community and global level" />
 
-        <Headshot 
-          imageSrc={SSAPLogo} 
+        <Headshot
+          imageSrc={SSAPLogo}
           imageAlt="Student Sustainability Association at Penn (SSAP)"
           name="Student Sustainability Association at Penn (SSAP)"
           description="The official umbrella group of environmental sustainability student groups on Penn's campus" />
 
-        <Headshot 
-          imageSrc={PUCPLogo} 
+        <Headshot
+          imageSrc={PUCPLogo}
           imageAlt="Penn Undergraduate Capital Partners"
           name="Penn Undergraduate Capital Partners"
           description="Student-led organization that is changing the way venture capital interacts with universities in Philadelphia and beyond." />
@@ -244,7 +244,7 @@ const People = () => {
       <Helmet defaultTitle="PCV Prize" titleTemplate="%s | PCV Prize">
         <title defer={false}>People</title>
         <meta charSet="utf-8" />
-        <link rel="canonical" href={"https://www.prize.pennclimateventures.org/people"} />
+        <link rel="canonical" href={"https://www.prize.pennclimateventures.org/2021/people"} />
         <meta name="description" content="Penn Climate Ventures official prize people." />
       </Helmet>
       <Navbar />
