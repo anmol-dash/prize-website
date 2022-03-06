15d22ff5855f1cfd11e563dec8c739879d027959
ryoma <84463842+hryoma@users.noreply.github.com>
2022-03-06 17:59:06 -0500

    Added 2021 judges

---
+++ src/pages/2021/judges.js
@@ -0,0 +1,130 @@
+import React from "react"
+import s from "styled-components"
+import Helmet from "react-helmet"
+import "../styles.scss"
+import { Title, Text, Subtitle} from "../../components/shared/Typography"
+import { Navbar, Footer } from "../../components"
+import Headshot from "../../components/shared/Headshot"
+
+import Lidiya from "../../images/2021/judges/LidiyaDervisheva.jpg"
+import Seth from "../../images/2021/judges/SethBannon.jpg"
+import Marisa from "../../images/2021/judges/MarisaSweeney.jpg"
+import Sam from "../../images/2021/judges/SamBursten.jpg"
+import Dennis from "../../images/2021/keynote/DennisWoodside.jpg";
+import Arun from "../../images/2021/keynote/ArunMajumdar.jpg";
+
+const GridLayout = s.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+// Prizes
+const People = () => {
+  return (
+    <div css={`text-align: center;
+                  width: 70vw;
+                  padding: 3vw;
+                  display: block;
+                  margin: 1vw auto`}>
+      <GridLayout>
+        <Headshot
+            imageSrc={Dennis}
+            imageAlt={"Dennis"}
+            name={"Dennis Woodside"}
+            description={<>President <br/> @ Impossible Foods</>}
+            width="190px"/>
+        <Headshot
+          imageSrc={Arun}
+          imageAlt={"Arun"}
+          name={"Dr. Arun Majumdar"}
+          description={<>Founding Director <br/> @ ARPA-E <br/> Former VP of Energy <br/> @ Google</>}
+          width="190px"/>
+        <Headshot
+          imageSrc={Marisa}
+          imageAlt={"Marisa"}
+          name={"Marisa Sweeney"}
+          description={<>Director <br/> @ Generate Capital</>}
+          width="190px"/>
+        <Headshot
+          imageSrc={Lidiya}
+          imageAlt={"Lidiya"}
+          name={"Lidiya Dervisheva"}
+          description={<>Partner <br/> @ Next47</>}
+          width="190px"/>
+      </GridLayout>
+    </div>
+  )
+}
+
+const Finals = () => (
+  <div css={`text-align: center;
+                width: 70vw;
+                padding: 3vw;
+                display: block;
+                margin: 1vw auto`}>
+    <Text fontSize="2rem" roboto bold>Live Finals, with keynote from</Text>
+    <div css={`display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    padding: 2vw;`}>
+      <Headshot
+        imageSrc={Dennis}
+        imageAlt={"Dennis"}
+        name={"Dennis Woodside"}
+        description={"President of Impossible Foods"}/>
+    </div>
+
+    <Text fontSize="2rem" roboto bold>and judging by</Text>
+
+    <GridLayout>
+      <Headshot
+        imageSrc={Lidiya}
+        imageAlt={"Lidiya Dervisheva"}
+        name={"Lidiya Dervisheva"}
+        description={<>Partner <br/> @ Next47</>}
+        width="190px"/>
+      <Headshot
+        imageSrc={Seth}
+        imageAlt={"Seth Bannon"}
+        name={"Seth Bannon"}
+        description={<>Founding Partner <br/> @ Fifty Years</>}
+        width="190px"/>
+      <Headshot
+        imageSrc={Marisa}
+        imageAlt={"Marisa Sweeney"}
+        name={"Marisa Sweeney"}
+        description={<>Director <br/> @ Generate Capital</>}
+        width="190px"/>
+      <Headshot
+        imageSrc={Sam}
+        imageAlt={"Sam Bursten"}
+        name={"Sam Bursten"}
+        description={<>Vice President <br/>@ Energy Impact Partners</>}
+        width="190px"/>
+    </GridLayout>
+  </div>
+)
+
+const Judges = () => {
+  return (
+    <>
+      <Helmet defaultTitle="PCV Prize" titleTemplate="%s | PCV Prize">
+        <title defer={false}>Judges</title>
+        <meta charSet="utf-8" />
+        <link rel="canonical" href={"https://www.prize.pennclimateventures.org/2021/people"} />
+        <meta name="description" content="Penn Climate Ventures official prize people." />
+      </Helmet>
+      <Navbar />
+      <div css={`padding: 90px 12vw;`}>
+        <Title css={`margin-bottom: 15px`}>Judges (2021)</Title>
+        <People/>
+        <br/>
+        <Finals/>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Judges;
