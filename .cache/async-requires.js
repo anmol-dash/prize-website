6a4aec011971584e4eaa6180d5ea5042d823bf07
anmol-dash <115905646+anmol-dash@users.noreply.github.com>
2024-02-29 16:42:22 -0500

    test1

---
+++ .cache/async-requires.js
@@ -0,0 +1,17 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---src-pages-2021-judges-js": () => import("./../../../src/pages/2021/judges.js" /* webpackChunkName: "component---src-pages-2021-judges-js" */),
+  "component---src-pages-2021-people-js": () => import("./../../../src/pages/2021/people.js" /* webpackChunkName: "component---src-pages-2021-people-js" */),
+  "component---src-pages-2021-sponsors-js": () => import("./../../../src/pages/2021/sponsors.js" /* webpackChunkName: "component---src-pages-2021-sponsors-js" */),
+  "component---src-pages-2021-winners-js": () => import("./../../../src/pages/2021/winners.js" /* webpackChunkName: "component---src-pages-2021-winners-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-people-js": () => import("./../../../src/pages/people.js" /* webpackChunkName: "component---src-pages-people-js" */),
+  "component---src-pages-schedule-js": () => import("./../../../src/pages/schedule.js" /* webpackChunkName: "component---src-pages-schedule-js" */),
+  "component---src-pages-sponsors-js": () => import("./../../../src/pages/sponsors.js" /* webpackChunkName: "component---src-pages-sponsors-js" */),
+  "component---src-pages-winners-js": () => import("./../../../src/pages/winners.js" /* webpackChunkName: "component---src-pages-winners-js" */),
+  "component---src-templates-page-template-js": () => import("./../../../src/templates/pageTemplate.js" /* webpackChunkName: "component---src-templates-page-template-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,20 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-2021-judges-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\2021\\judges.js"))),
+  "component---src-pages-2021-people-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\2021\\people.js"))),
+  "component---src-pages-2021-sponsors-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\2021\\sponsors.js"))),
+  "component---src-pages-2021-winners-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\2021\\winners.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\index.js"))),
+  "component---src-pages-people-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\people.js"))),
+  "component---src-pages-schedule-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\schedule.js"))),
+  "component---src-pages-sponsors-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\sponsors.js"))),
+  "component---src-pages-winners-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\pages\\winners.js"))),
+  "component---src-templates-page-template-js": hot(preferDefault(require("C:\\Users\\anmol\\OneDrive\\Anmol-OneDriveDocs\\Documents\\GitHub\\prize-website\\src\\templates\\pageTemplate.js")))
+}
+
